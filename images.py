16817f6fba34170c4f708be83849e9b282763cb6
Cris Vizcaino <cris.vizcaino@live.com>
2020-08-27 19:51:56 -0700

    first commit

---
+++ images.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageFilter
+
+img = Image.open('./Pokedex/pikachu.jpg')
+#filtered_img = img.filter(ImageFilter.EMBOSS)
+#convert image to grey scale
+filtered_img = img.convert('L')
+#saved modiefied image
+filtered_img.save("blurPika.png", 'png')
+filtered_img.show()
